a178bb922e54d8b99901361d7c00921e97710787
jitsuma <71244364+jitsuma@users.noreply.github.com>
2020-12-28 11:56:28 +0800

    Add files via upload

--- apartments/spiders/apartment_list.py
+++ apartments/spiders/apartment_list.py
@@ -6,44 +6,9 @@ class ApartmentListSpider(scrapy.Spider):
     allowed_domains = ['https://www.apartments.com']
     
     start_urls = [
-        'https://www.apartments.com/warren-oh-44485/1000-to-2000',
-        'https://www.apartments.com/portland-or-97210/1000-to-2000',
-        'https://www.apartments.com/chicago-il-60619/1000-to-2000',
-        'https://www.apartments.com/brooklyn-park-mn-55443/1000-to-2000',
-        'https://www.apartments.com/philadelphia-pa-19134/1000-to-2000',
-        'https://www.apartments.com/brooklyn-park-mn-55428/1000-to-2000',
-        'https://www.apartments.com/brooklyn-ny-11218/1000-to-2000',
-        'https://www.apartments.com/jamaica-ny-11432/1000-to-2000',
-        'https://www.apartments.com/calexico-ca-92231/1000-to-2000',
-        'https://www.apartments.com/clarkston-ga-30021/1000-to-2000',
-        'https://www.apartments.com/lakeland-fl-33805/1000-to-2000',
-        'https://www.apartments.com/santa-clarita-ca-91321/1000-to-2000',
-        'https://www.apartments.com/philadelphia-pa-19147/1000-to-2000',
-        'https://www.apartments.com/atlanta-ga-30316/1000-to-2000',
-        'https://www.apartments.com/indianapolis-in-46201/1000-to-2000',
-        'https://www.apartments.com/union-city-ga-30291/1000-to-2000',
-        'https://www.apartments.com/san-antonio-tx-78207/1000-to-2000',
-        'https://www.apartments.com/clinton-township-mi-48038/1000-to-2000',
-        'https://www.apartments.com/philadelphia-pa-19131/1000-to-2000',
-        'https://www.apartments.com/columbia-sc-29201/1000-to-2000',
-        'https://www.apartments.com/tulsa-ok-74105/1000-to-2000',
-        'https://www.apartments.com/tulsa-ok-74133/1000-to-2000',
-        'https://www.apartments.com/pomona-ca-91767/1000-to-2000',
-        'https://www.apartments.com/long-beach-ca-90804/1000-to-2000',
-        'https://www.apartments.com/charlotte-nc-28205/1000-to-2000',
-        'https://www.apartments.com/louisville-ky-40216/1000-to-2000',
-        'https://www.apartments.com/west-new-york-nj-07093/1000-to-2000',
-        'https://www.apartments.com/cleveland-oh-44119/1000-to-2000',
-        'https://www.apartments.com/indianapolis-in-46226/1000-to-2000',
-        'https://www.apartments.com/hiram-ga-30141/1000-to-2000',
-        'https://www.apartments.com/mableton-ga-30126/1000-to-2000',
-        'https://www.apartments.com/ozone-park-ny-11416/1000-to-2000',
-        'https://www.apartments.com/cincinnati-oh-45208/1000-to-2000',
-        'https://www.apartments.com/eden-nc-27288/1000-to-2000',
-        'https://www.apartments.com/houston-tx-77477/1000-to-2000',
-        'https://www.apartments.com/tampa-fl-33614/1000-to-2000',
-        'https://www.apartments.com/fraser-mi-48026/1000-to-2000',
-        'https://www.apartments.com/sterling-heights-mi-48312/1000-to-2000'
+        'https://www.apartments.com/buffalo-ny-14211/1000-to-2000',
+        'https://www.apartments.com/mcdonough-ga-30253/1000-to-2000'
+
 
 
             ]
